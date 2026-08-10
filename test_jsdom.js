e4281f090a5727c722e7cbef58b97679ab91fdcb
ARK DURRANI <5073340abdulrehmankhandurrani@gmail.com>
2026-08-10 08:13:34 +0530

    chore: remove node_modules and add gitignore

--- test_jsdom.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const html = fs.readFileSync('frontend/dashboard.html', 'utf8');
-const dom = new JSDOM(html, { runScripts: "dangerously", url: "http://localhost/" });
